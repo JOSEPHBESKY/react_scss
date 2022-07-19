399eec6f450189241a8a57631912dbd8908cda94
JOSEPH BESKY J <104523855+JOSEPHBESKY@users.noreply.github.com>
2022-07-19 08:30:31 +0100

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React from 'react'
+
+export default function app(){
+  return (
+    <div className='container'>
+      <div className='nav'>
+        {img src="logo192.png" width={'100px'} className='logo'></div>}
+        <a href=''>Home</a>
+        <a href=''>About</a>
+        <a href=''>Services</a>
+        <a href=''>Feedback</a>
+        <div className='social'>
+          <a href=''>Facbook</a>
+          <a href=''>Instragram</a>
+          <a href=''>Twitter</a>
+        </div>
+      </div>
+      <div className="container2">
+        <div className='left'>
+        </div>
+        <div className='right'>
+          {<img src="logo192.png" width={"100px"}></img>}
+        </div>
+      </div>
+      <div className='thirdsec'></div>
+    </div>
+  )
+}
